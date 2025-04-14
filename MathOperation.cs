2e6fb02f0f8206640670151c64ffe436f1681151
alexscespedes <alexandersenc99@gmail.com>
2025-04-14 19:21:34 -0400

    Add main message & math operation class

---
+++ MathOperation.cs
@@ -0,0 +1,21 @@
+namespace SimpleCalculator
+{
+    public class MathOperation {
+
+        public double Addition(int x, int y) {
+            return x + y;
+        }
+
+        public double Subtraction(int x, int y) {
+            return x - y;
+        }
+
+        public double Multiplication(int x, int y) {
+            return x * y;
+        }
+
+        public double Division(int x, int y) {
+            return x / y;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+                Console.WriteLine("Welcome to the Simple Calculator Console App");
+                Console.WriteLine("1 for Addition");
+                Console.WriteLine("2 for Subtraction");
+                Console.WriteLine("3 for Multiplication");
+                Console.WriteLine("4 for Division");
+                Console.Write("Choose an option: ");
         }
     }
 }
